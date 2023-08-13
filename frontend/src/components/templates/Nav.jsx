eeb71a07fadf76b257b3bdd8ec09b9aa607ab77f
rmatos <ronison.matos@neogrid.com>
2023-08-13 00:16:17 -0300

    RivoLink - Melhorias no menu de navegação

--- frontend/src/components/templates/Nav.jsx
+++ frontend/src/components/templates/Nav.jsx
@@ -1,21 +1,34 @@
 import "./Nav.css";
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 
-const Nav = (props) => (
-  <aside className="menu-area">
-    <nav className="menu">
-      <Link to="/">
-        <i className="fa fa-home"></i> Início
-      </Link>
-      <Link to="/users">
-        <i className="fa fa-users"></i> Usuários
-      </Link>
-      <Link to="/schedule">
-        <i className="fa fa-calendar"></i> Agenda
-      </Link>
-    </nav>
-  </aside>
-);
+const Nav = (props) => {
+  const [selectedButton, setSelectedButton] = useState('');
+
+  const handleButtonClick = (buttonId) => {
+    setSelectedButton(buttonId);
+  };
+  return(
+      <aside className="menu-area">
+        <nav className="menu">
+          <Link to="/"
+                className={`menu-button ${selectedButton === '' ? 'selected' : ''}`}
+                onClick={() => handleButtonClick('')}>
+            <i className="fa fa-home"></i> Início
+          </Link>
+          <Link to="/users"
+                className={`menu-button ${selectedButton === 'users' ? 'selected' : ''}`}
+                onClick={() => handleButtonClick('users')}>
+            <i className="fa fa-users"></i> Usuários
+          </Link>
+          <Link to="/schedule"
+                className={`menu-button ${selectedButton === 'schedule' ? 'selected' : ''}`}
+                onClick={() => handleButtonClick('schedule')}>
+            <i className="fa fa-calendar"></i> Agenda
+          </Link>
+        </nav>
+      </aside>
+  );
+};
 
 export default Nav;
